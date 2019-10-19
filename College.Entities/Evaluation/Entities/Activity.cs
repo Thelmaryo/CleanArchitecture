903aa9af1505634d7b1a36434b743dd58e30f087
Thelmaryo Vieira Lima <thelmaryo@hotmail.com>
2019-10-19 19:06:28 -0300

    Entities layer structure

---
+++ College.Entities/Evaluation/Entities/Activity.cs
@@ -0,0 +1,21 @@
+﻿using College.Entities.Shared;
+using System;
+
+namespace College.Entities.Evaluation.Entities
+{
+    public class Activity : Entity
+    {
+        public Activity(Discipline discipline, string description, decimal value, DateTime date)
+        {
+            Discipline = discipline;
+            Description = description;
+            Value = value;
+            Date = date;
+        }
+
+        public Discipline Discipline { get; private set; }
+        public string Description { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime Date { get; private set; }
+    }
+}
---
+++ College.Entities/Evaluation/Entities/Discipline.cs
@@ -0,0 +1,19 @@
+﻿using College.Entities.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College.Entities.Evaluation.Entities
+{
+    public class Discipline : Entity
+    {
+        public Discipline(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+}
